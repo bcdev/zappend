38f6027be7fe4969825322903d04a67a7ce07074
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-03-12 13:14:32 +0100

    Updated copyright

--- tests/fsutil/__init__.py
+++ tests/fsutil/__init__.py
@@ -1,3 +1,3 @@
-# Copyright © 2024 Norman Fomferra
+# Copyright © 2024 Norman Fomferra and contributors
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
